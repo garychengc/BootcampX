67146e3aa5204a51a1c9dd1e62a7a58af7c776df
Gary Chen <garychen.whc@gmail.com>
2019-09-17 00:59:13 +0000

    completed the first 5 questions in queries exercise 4, exercise is not done yet

---
+++ 4_queries/2_total_student_assistance_requests.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(assistance_requests. *) AS total_assistances, students.name AS name
+FROM students JOIN assistance_requests ON assistance_requests.student_id = students.id
+WHERE students.name = 'Elliot Dickinson'
+GROUP BY students.name;
